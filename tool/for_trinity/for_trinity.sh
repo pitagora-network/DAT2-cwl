59f8ab911ce04c635609deb70f9f09b8b250952b
yyoshiaki <y.yasumizu@gmail.com>
2019-06-18 22:47:09 +0900

    for_trinity

--- tool/for_trinity/for_trinity.sh
+++ tool/for_trinity/for_trinity.sh
@@ -7,5 +7,7 @@ SRR=`echo ${IN1} | grep -o ".RR[0-9]*"`
 OUT1=${SRR}_1.fq.gz
 OUT2=${SRR}_2.fq.gz
 
+wget https://raw.githubusercontent.com/bonohu/denovoTA/master/for_trinity.pl 
+
 gzip -cd ${IN1} | perl for_trinity.pl 1 | gzip -c > ${OUT1}
 gzip -cd ${IN2} | perl for_trinity.pl 2 | gzip -c > ${OUT2}
