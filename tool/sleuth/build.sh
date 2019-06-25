aba78e263283f0527b1cee98640af5c425732943
ykohki <43834721+ykohki@users.noreply.github.com>
2019-06-25 12:22:30 +0900

    Update build.sh

--- tool/sleuth/build.sh
+++ tool/sleuth/build.sh
@@ -4,3 +4,8 @@ cd .
 
 #wget https://raw.githubusercontent.com/bonohu/denovoTA/master/for_trinity.pl
 docker image build -t dat2-cwl/sleuth .
+
+#コンテナを起動
+docker container run --name sleuth -itd dat2-cwl/sleuth
+
+#docker container cp target2gene:target2gene.txt .
