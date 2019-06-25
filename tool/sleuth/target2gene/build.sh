ef7d35309fbe8412039102c7685379c054d497dc
ykohki <43834721+ykohki@users.noreply.github.com>
2019-06-25 11:41:22 +0900

    Update build.sh

--- tool/sleuth/target2gene/build.sh
+++ tool/sleuth/target2gene/build.sh
@@ -4,3 +4,7 @@ cd .
 
 #wget https://raw.githubusercontent.com/bonohu/denovoTA/master/for_trinity.pl
 docker image build -t dat2-cwl/target2gene .
+#コンテナを起動
+docker container run --name target2gene -itd dat2-cwl/target2gene
+
+docker container cp target2gene:target2gene.txt .
