b14c2edf04c0d36338569d519e1f60bc33daf59f
ykohki <43834721+ykohki@users.noreply.github.com>
2019-06-26 16:38:26 +0900

    add

--- tool/sleuth/build.sh
+++ tool/sleuth/build.sh
@@ -5,6 +5,7 @@ cd .
 cp /home/kyamada/cwl/data_cwl/sample.txt .
 cp /home/kyamada/cwl/data_cwl/target2gene.txt .
 cp -r /home/kyamada/cwl/out_test/kallisto/kallisto_quant_pe/SRR1550989 .
+cp -r /home/kyamada/cwl/out_test/kallisto/kallisto_quant_pe/SRR1551091 .
 
 #wget https://raw.githubusercontent.com/bonohu/denovoTA/master/for_trinity.pl
 docker image build -t dat2-cwl/sleuth .
