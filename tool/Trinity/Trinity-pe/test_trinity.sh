ef252312ada75eeb4c1fd0342dc1609fbbd5b1b8
suecharo <suehiro619@gmail.com>
2022-03-23 18:43:52 +0900

    Move job file to tool dir

--- tool/Trinity/Trinity-pe/test_trinity.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eux
-
-wget https://github.com/trinityrnaseq/trinityrnaseq/raw/devel/sample_data/test_Trinity_Assembly/reads.right.fq.gz
-wget https://github.com/trinityrnaseq/trinityrnaseq/raw/devel/sample_data/test_Trinity_Assembly/reads.left.fq.gz
-
-cwltool --debug Trinity-pe.cwl Trinity-pe.yaml
