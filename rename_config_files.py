69e7c0479af00695127e402962e1d81b6b8142df
suecharo <suehiro619@gmail.com>
2022-05-12 08:49:52 +0000

    Remove

--- rename_config_files.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-here = Path(__file__).parent
-files = here.glob('**/yevis-config.yml')
-for file in files:
-    di = file.parent
-    new_file = di.joinpath("yevis-metadata.yml")
-    file.rename(new_file)
